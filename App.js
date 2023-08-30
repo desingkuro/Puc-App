cbf8aad9fd9f798b86ab3978280b2e120da17150
desingkuro <andresmauricio745gmail.com>
2023-08-30 08:16:27 -0500

    actualizacion de paquetes

--- App.js
+++ App.js
@@ -4,14 +4,19 @@ import {NavigationContainer} from '@react-navigation/native'
 import { MyTabs } from './Navigation/TabNavigationComponent';
 import {ContextContainer} from './Context/Context'
 
+import { RNCSafeAreaProvider } from 'react-native-safe-area-context';
+
 export default function App() {
   return (
+    <RNCSafeAreaProvider>
+
     <NavigationContainer>
       <ContextContainer>
         <MyTabs/>
         <StatusBar style="auto" />
       </ContextContainer>
     </NavigationContainer>
+    </RNCSafeAreaProvider>
   );
 }
 
--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
+import { Ionicons } from '@expo/vector-icons';
 
 export function SettingsScreen(){
     return(
-        <Text>Settings</Text>
+        <View style={estilos.container}>
+            <Ionicons name="settings" size={120} color="grey" />
+            <Text style={estilos.text}>Esta app esta en proceso de desarrollo y de crecimiento, si encuentar alguna falla o deficis por favor hacerlo saber al siguiente contacto</Text>
+            <Text style={estilos.text}>desingkuro@gmail.com</Text>
+        </View>
     )
 }
+const estilos=StyleSheet.create({
+    container:{
+        alignItems:'center',justifyContent:'center',flex:1,
+        gap:5,
+        opacity:0.7
+    },
+    text:{
+        color:'grey',
+        textAlign:'center'
+    }
+})
